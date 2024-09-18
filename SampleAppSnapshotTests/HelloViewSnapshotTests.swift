e982a04ac808db99bef6a1d4d847452fc65a27c0
maiyama <myam0118@gmail.com>
2024-09-18 13:40:36 +0900

    Fix precision

--- SampleAppSnapshotTests/HelloViewSnapshotTests.swift
+++ SampleAppSnapshotTests/HelloViewSnapshotTests.swift
@@ -38,7 +38,7 @@ struct HelloViewSnapshotTests {
                     of: UIHostingController(rootView: preview.content),
                     as: .image(
                         on: device.viewImageConfig,
-                        precision: 0.80,
+                        precision: 1,
                         perceptualPrecision: 0.97
                     ),
                     named: "HelloView-\(device)",
